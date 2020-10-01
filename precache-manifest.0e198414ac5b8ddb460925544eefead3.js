a24d70ed21f0c62a5ef65df4d52811cdcead452d
Indushree <indushree95indu@gmail.com>
2020-10-01 23:16:50 +0530

    Updates

---
+++ precache-manifest.0e198414ac5b8ddb460925544eefead3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cec69f5f07406491cd4",
+    "url": "/react-counter/static/css/main.22215ffb.chunk.css"
+  },
+  {
+    "revision": "0cec69f5f07406491cd4",
+    "url": "/react-counter/static/js/main.0cec69f5.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-counter/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7e40d00a0704eff0a11f",
+    "url": "/react-counter/static/js/runtime~main.7e40d00a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-counter/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "296c20b8abac68acda6c916bc49329b9",
+    "url": "/react-counter/index.html"
+  }
+];
